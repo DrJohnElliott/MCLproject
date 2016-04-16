c06507c4cc7fa757e40435847ff337eff36d4faa
DrJohnElliott <DrJohnElliott.github@gmail.com>
2016-04-16 10:54:02 -0400

    Refined and working

--- MCLChemReport.R
+++ MCLChemReport.R
@@ -1,25 +1,22 @@
 
 library(dplyr)
+
+
 setwd("C:/Users/John/Desktop/R-Code/Projects/MCL Reports Chromalloy")
 
-reportdata <- read.table("BMS1008_IMR_Chem_Main_201600351.txt",  header = FALSE)
+#DATA Vars
+        labNames <- c("IMR", "NSL")
+# Read Text File into single varible table each line is a charecter string
+        myData <- read.csv("BMS1008_IMR_Chem_Main_201600351.txt", sep = "/", header = FALSE, fileEncoding="UTF-8-BOM",colClasses = "character")
+#Find Lab Name
+        myLab <- myData$V1[1]
+        str_match(myLab,labNames)
+        labNames %in%  myLab
+        
+#Determine Lab
+        
+        substring1="CHEMISTRY|^1"
+        Chem_start_stop <-  grep(substring1,myData$V1)
+        my_chem <- myData[c(Chem_start_stop[1]+2):c(Chem_start_stop[2]-1),]
 
-A <- readLines(con <- file("BMS1008_IMR_Chem_Main_201600351.txt", encoding = "UTF-8"),skipNul = TRUE)
-close(con)
-
-myData <- read.csv("BMS1008_IMR_Chem_Main_201600351.txt", sep = "/", header = FALSE, fileEncoding="UTF-8-BOM")
-#unique(Encoding(A)) # will most likely be UTF-8
-myChem <- read.table("BMS1008_IMR_Chem_Main_201600351.csv", sep = ",", header = TRUE)
-myMech<- read.table("BMS1008_Mech_IMR_201603099Chormally.csv" ,sep = ",", header = TRUE)
-#Make List of Column Names with Activity, Subject, Mean and Standard Deveation Data 
-#FindList <- names(my_data)
-substring="CHEMISTRY|^1"
-Chemistry_filter <-  grepl(substring,A)
-chem_rows <- which(Chemistry_filter)
-myNum <- as.integer(chem_rows[2]-chem_rows[1])
  
-my_chem <- readLines(A, skip = chem_rows[1] ,  )
-final_names1 <- my_names[Chemistry_filter]
-
-
-final_names <- (c( "Activity", "Subject", final_names1) )
